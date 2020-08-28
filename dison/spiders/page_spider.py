25b0ca0e103c60fcd5dc0a5f439acc778bbed96c
isebarn <isebarn182@gmail.com>
2020-08-28 14:20:17 +0000

    comparison logic

--- dison/spiders/page_spider.py
+++ dison/spiders/page_spider.py
@@ -106,7 +106,7 @@ class RootSpider(scrapy.Spider):
     elif response.xpath("//ul/li[@class='a-selected']/following-sibling::li/@href").extract_first(None) != None:
       next_page = marketplace.Value + response.xpath("//ul/li[@class='a-selected']/following-sibling::li/@href").extract_first(None)
 
-    elif len(response.xpath("//ul/li[@class='a-normal']/@href").extract()) != None:
+    elif len(response.xpath("//ul/li[@class='a-normal']/@href").extract()) > 0:
       next_page = marketplace.Value + response.xpath("//ul/li[@class='a-normal']/@href").extract()[-1]
 
     Operations.UpdatePageSearch({'id': response.meta.get('root'), 'value': next_page})
